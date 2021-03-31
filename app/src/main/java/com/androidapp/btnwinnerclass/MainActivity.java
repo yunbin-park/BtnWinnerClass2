050549c1eb518e84fcb498f21c8565b36ed96368
Y-B Park <201921055@sangmyung.kr>
2021-03-31 15:06:37 +0900

    inner class with this

--- app/src/main/java/com/androidapp/btnwinnerclass/MainActivity.java
+++ app/src/main/java/com/androidapp/btnwinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.androidapp.btnwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnClickLstener(this));
+    }
+
+    private class MyOnClickLstener implements View.OnClickListener {
+        MainActivity mainActivity;
+        public MyOnClickLstener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onClick(View view) {
+            mainActivity.mTextView1.setText("You clicked button 1");
+        }
     }
 }

2f5e29f5b5b32b62d6c063a1d2a77cb845627acb
Y-B Park <201921055@sangmyung.kr>
2021-03-31 16:01:44 +0900

    innder class for multiple buttons

--- app/src/main/java/com/androidapp/btnwinnerclass/MainActivity.java
+++ app/src/main/java/com/androidapp/btnwinnerclass/MainActivity.java
@@ -3,11 +3,13 @@ package com.androidapp.btnwinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button mButton1;
+    Button mButton2;
     TextView mTextView1;
 
     @Override
@@ -16,8 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickLstener(this));
+        mButton1.setOnClickListener(new MyOnClickLstener());
+        mButton2.setOnClickListener(new MyOnClickLstener());
+    }
+
+    private class MyOnClickLstener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button1: mTextView1.setText("You clicked button1");
+                case R.id.button2: mTextView1.setText("You clicked button2");
+                default: break;
+            }
+        }
     }
 }
